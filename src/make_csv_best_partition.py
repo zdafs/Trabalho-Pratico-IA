c514e8318b48ab068a5e6bb3717ed39206b3c148
Lucca Carvalho <luccalafonte97@gmail.com>
2019-01-19 17:19:49 -0200

    Adicionado graficos

--- src/make_csv_best_partition.py
+++ src/make_csv_best_partition.py
@@ -3,10 +3,10 @@ from pyexcel_ods import save_data
 import collections
 
 data_vec = [['Grupo', 'd1', 'd2']]
-coordinates = Coordinate.coordinates_from_file('../datasets/c2ds1-2sp.txt')
-partitions = Partition.partitions_from_file('results/c2ds1-2sp/k_medias/c2ds1-2sp_k_medias_4_clusters.txt')
+coordinates = Coordinate.coordinates_from_file('../datasets/monkey.txt')
+partitions = Partition.partitions_from_file('results/monkey/k_medias/monkey_k_medias_7_clusters.txt')
 for i in range(len(partitions)):
     data_vec.append([partitions[i], coordinates[i].d1, coordinates[i].d2])
 data = collections.OrderedDict()
 data.update({"Planilha 1": data_vec})
-save_data('c2ds1-2sp_k_medias_4_clusters.ods', data)
+save_data('monkey_k_medias_7_clusters.ods', data)
